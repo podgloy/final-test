a568e3e1007dffa570e6dc3d5ec9865b5b2106b7
podgloy <nawarat.saetang@hotmail.com>
2024-05-27 18:21:17 +0700

    del

--- src/app/api/create-img/route.js
+++ src/app/api/create-img/route.js
@@ -4,11 +4,6 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
-// export function GET(){
-//   return Response.json({
-//     status: "ok",
-//   });
-// }
 
 export async function POST(req) {
 
